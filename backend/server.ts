fc2d8576f1cf758fd95679d07c39da40fecb1f6f
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-10-28 16:45:07 +0200

    routes created

--- backend/server.ts
+++ backend/server.ts
@@ -1,1 +1,11 @@
-console.log('hip hip hura');
+import * as express from 'express';
+import { PORT } from './utils/config';
+
+const app = express();
+app.use(express.json()); //app ca use json
+
+app.use('/api/projects', require('./routes/projectRoutes'));
+
+app.listen(PORT, () =>
+  console.log(`Server is ON and running on port: http://localhost:${PORT}`)
+);
