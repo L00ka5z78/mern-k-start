4c6ff8528e742a98eb41f83786b234391da14575
Lukasz Bolach <bravo1_lima1@yahoo.com>
2022-10-28 17:55:56 +0200

    errorHandler created

--- backend/server.ts
+++ backend/server.ts
@@ -1,10 +1,13 @@
 import * as express from 'express';
+import { errorHandler } from './middleware/errorMiddleware';
 import { PORT } from './utils/config';
 
 const app = express();
 app.use(express.json()); //app ca use json
 
 app.use('/api/projects', require('./routes/projectRoutes'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () =>
   console.log(`Server is ON and running on port: http://localhost:${PORT}`)
